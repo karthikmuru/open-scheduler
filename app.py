818560d6621b284039a661e3b51b16dc2738a5ea
karthikmuru <karthik.murugesan@outlook.in>
2023-03-25 21:10:39 -0700

    Adding Database connection + code refactor

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ from app import create_app
 from config import config
 import os
 
-# Get the configuration based on the FLASK_ENV environment variable
 app_config = config[os.environ.get('FLASK_ENV', 'default')]
 
-# Create the Flask app
 app = create_app(config_object=app_config)
 
 if __name__ == '__main__':
